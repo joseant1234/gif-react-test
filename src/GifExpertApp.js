52e023c9739cabf886a03de5fee8258e4536970c
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-09-09 21:09:45 -0500

    adding categories for handleAdd

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    // const categories = ['Naruto', 'Caballeros del zodiaco', 'Dragon ball']
+    const [categories, setCategories] = useState(['Naruto', 'Caballeros del zodiaco', 'Dragon ball']);
+
+    const handleAdd = () => {
+        // setCategories([...categories, 'Anime']);
+        setCategories(cats => [...cats, 'Anime']);
+    }
+
     return (
         <>
             <h2>GiExpertApp</h2>
             <hr/>
+            <button onClick={handleAdd}>Agregar</button>
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
         </>
     )
 }
