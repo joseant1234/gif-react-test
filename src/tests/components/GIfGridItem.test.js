f76854db4a19af8c031c6aaf2d573d360094fc45
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-09-18 21:30:16 -0500

    adding proptypes in GifGridItem component

--- src/tests/components/GIfGridItem.test.js
+++ src/tests/components/GIfGridItem.test.js
@@ -4,8 +4,11 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Test in <GidGridItem />', () => {
 
+    const title = 'A title';
+    const url = 'https://localhost/image.jpg';
+
     test('should show the component correctly', () => {
-        const wrapper = shallow(<GifGridItem />)
+        const wrapper = shallow(<GifGridItem title={title} url={url} />)
         expect(wrapper).toMatchSnapshot();
 
     })
