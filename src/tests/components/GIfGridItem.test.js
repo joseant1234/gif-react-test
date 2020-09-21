486591da1303fa546d7da5f5e29f8cf3b8aac6d3
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-09-21 12:36:55 -0500

    adding test to gif grid item

--- src/tests/components/GIfGridItem.test.js
+++ src/tests/components/GIfGridItem.test.js
@@ -6,10 +6,34 @@ describe('Test in <GidGridItem />', () => {
 
     const title = 'A title';
     const url = 'https://localhost/image.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />)
 
     test('should show the component correctly', () => {
-        const wrapper = shallow(<GifGridItem title={title} url={url} />)
+
         expect(wrapper).toMatchSnapshot();
 
+    })
+
+    test('should have a p tag with the title property', () => {
+
+        const p = wrapper.find('p');
+
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('should get a image equal to url and alt of the props', () => {
+
+        const img = wrapper.find('img');
+
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    })
+
+    test('should have animate__fadeIn class', () => {
+
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect(className.includes('animate__fadeIn')).toBe(true);
     })
 })
