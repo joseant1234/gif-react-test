18ee29b7441dfd2051a70a9283a59f8819e7a3ee
Jose Tafur <jose.antonio.umeres@gmail.com>
2020-09-26 08:51:44 -0500

    adding test in custom hook

--- src/tests/hooks/useFetchGifs.js
+++ src/tests/hooks/useFetchGifs.js
@@ -3,14 +3,28 @@ const { renderHook } = require("@testing-library/react-hooks");
 
 describe('Test in useFetchGifs hook', () => {
 
-    test('should return the initial state', () => {
+    test('should return the initial state', async() => {
 
         // crea un componente virtual y coloca el custom hook
-        const { result } = renderHook(() =>  useFetchGifs(category) );
+        const { result, waitForNextUpdate } = renderHook(() =>  useFetchGifs(category) );
         // el valor actual del custom hook
         const { data, loading } = result.current;
 
+        await waitForNextUpdate();
+
         expect(data).toEqual([]);
         expect(loading).toBe(true);
+    })
+
+    test('should return array of images', async() => {
+        // waitForNextUpdate espera una actualizacion en el estado
+        // la promesa indica cuando sucedió una actualización en el estado del custom hook
+        const { result, waitForNextUpdate } = renderHook(() =>  useFetchGifs(category) );
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect(data.length).toBe(10);
+        expect(loading).toBe(false);
     })
 })
